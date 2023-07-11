5f788d0d78712039dd4b9b55954f345f9310d34e
salman1256@yahoo.com <salman1256@yahoo.com>
2023-07-11 15:42:53 +0530

    DataTypes Example

---
+++ DataTypesExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ValueType
+            //Referenc Type
+            int num = 5;
+            Console.WriteLine("Number is "+num);
+            Console.ReadKey();
+
+        }
+    }
+}
